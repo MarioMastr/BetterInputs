9e542ca1e29bb654fa0be8ea256d573616293e98
MarioMastr <ashrmoha11@gmail.com>
2024-07-23 23:03:13 +0100

    some mac things (finally)

--- src/utils.hpp
+++ src/utils.hpp
@@ -8,6 +8,59 @@
 
 #ifdef GEODE_IS_WINDOWS
 #include <WinUser.h> // virtual keys
+#elif defined(GEODE_IS_MACOS)
+#include <CoreGraphics/CoreGraphics.h>
+enum {
+  Return                    = 0x24,
+  Tab                       = 0x30,
+  Space                     = 0x31,
+  Delete                    = 0x33,
+  Escape                    = 0x35,
+  Command                   = 0x37,
+  Shift                     = 0x38,
+  CapsLock                  = 0x39,
+  Option                    = 0x3A,
+  Control                   = 0x3B,
+  RightCommand              = 0x36,
+  RightShift                = 0x3C,
+  RightOption               = 0x3D,
+  RightControl              = 0x3E,
+  Function                  = 0x3F,
+  F17                       = 0x40,
+  VolumeUp                  = 0x48,
+  VolumeDown                = 0x49,
+  Mute                      = 0x4A,
+  F18                       = 0x4F,
+  F19                       = 0x50,
+  F20                       = 0x5A,
+  F5                        = 0x60,
+  F6                        = 0x61,
+  F7                        = 0x62,
+  F3                        = 0x63,
+  F8                        = 0x64,
+  F9                        = 0x65,
+  F11                       = 0x67,
+  F13                       = 0x69,
+  F16                       = 0x6A,
+  F14                       = 0x6B,
+  F10                       = 0x6D,
+  ContextualMenu            = 0x6E,
+  F12                       = 0x6F,
+  F15                       = 0x71,
+  Help                      = 0x72,
+  Home                      = 0x73,
+  PageUp                    = 0x74,
+  ForwardDelete             = 0x75,
+  F4                        = 0x76,
+  End                       = 0x77,
+  F2                        = 0x78,
+  PageDown                  = 0x79,
+  F1                        = 0x7A,
+  LeftArrow                 = 0x7B,
+  RightArrow                = 0x7C,
+  DownArrow                 = 0x7D,
+  UpArrow                   = 0x7E
+}; // we do this so that we don't have to include the entire carbon header, which causes a conflict with some enums (all the necessary keys are here anyways :D)
 #endif
 
 #include <Geode/cocos/cocoa/CCGeometry.h>
@@ -136,7 +189,10 @@ inline bool keyDown(PlatformKey key)
         case BI::PlatformKey::LEFT_SHIFT: return GetKeyState(VK_SHIFT) & 0x8000;
     }
 #elif defined(GEODE_IS_MACOS)
-    return false;
+    switch (key) {
+        case BI::PlatformKey::LEFT_CONTROL: return CGEventSourceKeyState(kCGEventSourceStateCombinedSessionState, Control);
+        case BI::PlatformKey::LEFT_SHIFT: return CGEventSourceKeyState(kCGEventSourceStateCombinedSessionState, Shift);
+    }
 #endif
 }
 } // namespace platform
